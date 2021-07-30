3695e0466d692fa23a1d518789872c4d7b7d1a03
Ulisses G <ulissesg2018@gmail.com>
2021-07-30 17:23:57 -0300

    back-end done

--- DoIt/urls.py
+++ DoIt/urls.py
@@ -6,8 +6,12 @@ from .views import IndexView
 app_name = 'DoIt'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('tasks/<int:pk>', views.ListTasksView.as_view(), name='tasks'),
-    path('<int:pk>/', views.DetailsTaskView.as_view(), name='details'),
-    path('new_list/', views.new_list, name='new_list'),
-    path('new_task/<int:pk>', views.new_task, name='new_task'),
+    path('-tasks/<int:pk>', views.ListTasksView.as_view(), name='tasks'),
+    path('-task/<int:pk>', views.DetailsTaskView.as_view(), name='details'),
+    path('-new-list/', views.NewListView.as_view(), name='new_list'),
+    path('-new-task/<int:pk>', views.NewTaskView.as_view(), name='new_task'),
+    path('-delete-list/<int:pk>', views.ListDeleteView.as_view(), name='list_delete'),
+    path('-delete-task/<int:pk>', views.TaskDeleteView.as_view(), name='task_delete'),
+    path('-edit-list/<int:pk>', views.ListEditView.as_view(), name='list_edit'),
+    path('-edit-task/<int:pk>', views.TaskEditView.as_view(), name='task_edit')
 ]
--- DoIt/views.py
+++ DoIt/views.py
@@ -1,9 +1,15 @@
 # Create your views here.
+import form as form
+from django import forms
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.admin.widgets import AdminDateWidget
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.template import loader
-from django.urls import reverse
+from django.template.defaultfilters import default
+from django.urls import reverse, reverse_lazy
+from django.utils.dateformat import DateFormat
 from django.views import generic
 
 from DoIt.models import List, Task
@@ -17,46 +23,105 @@ class IndexView(generic.ListView):
         return List.objects.all()
 
 
-def new_list(request):
-    if request.method == 'POST':
-        if request.POST['name']:
-            list_name = request.POST['name']
-            nw_list = List(name=list_name)
-            nw_list.save()
-
-            messages.info(request, 'List created successfully')
-            return HttpResponseRedirect(reverse('DoIt:index'))
-
-        else:
-            # Redisplay the form to create a list.
-            return render(request, 'DoIt/new_list.html', {
-                'error_message': "You didn't typed a name for the list.",
-            })
-
-    else:
-        template = loader.get_template('DoIt/new_list.html')
-        return HttpResponse(template.render({}, request))
-
-
 class ListTasksView(generic.ListView):
     template_name = 'DoIt/list_tasks.html'
-    context_object_name = 'task_of_list'
+    context_object_name = 'list_of_task'
 
     def get_queryset(self):
         return Task.objects.filter(list=self.kwargs.get('pk'))
 
 
-def new_task(request, pk):
-    if request.method == 'POST':
-        pass
+class NewListView(SuccessMessageMixin, generic.CreateView):
+    model = List
+    fields = '__all__'
 
-    else:
-        context = {
-            'list': List.objects.get(pk=pk),
+    def get_success_url(self):
+        messages.info(self.request, 'List ' + self.request.POST['name'] + ' created successfully')
+        return reverse_lazy('DoIt:index')
+
+    def get_context_data(self, **kwargs):
+        context = super(NewListView, self).get_context_data(**kwargs)
+        context['page_title'] = 'New List'
+        return context
+
+
+class ListEditView(SuccessMessageMixin, generic.UpdateView):
+    model = List
+    fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super(ListEditView, self).get_context_data(**kwargs)
+        context['page_title'] = 'Edit List ' + get_object_or_404(List, id=self.kwargs['pk']).name
+        return context
+
+    def get_success_url(self):
+        messages.info(self.request, 'List ' + self.request.POST['name'] + ' Edited')
+        return reverse_lazy('DoIt:index')
+
+
+class ListDeleteView(generic.DeleteView):
+    model = List
+    success_url = reverse_lazy('DoIt:index')
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('name', 'description', 'is_done', 'start_date', 'end_date', 'time_it_takes', 'is_important')
+        widgets = {
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
-        return render(request, 'DoIt/new_task.html', context)
+
+
+class NewTaskView(SuccessMessageMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+
+    def get_success_url(self):
+        messages.info(self.request, 'Task ' + self.request.POST['name'] + ' created successfully')
+        return reverse_lazy('DoIt:tasks', kwargs={
+            'pk': self.kwargs['pk']
+        })
+
+    def form_valid(self, form):
+        lst = get_object_or_404(List, id=self.kwargs['pk'])
+        form.instance.list = lst
+        return super(NewTaskView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(NewTaskView, self).get_context_data(**kwargs)
+        context['list'] = get_object_or_404(List, id=self.kwargs['pk'])
+        context['page_title'] = 'Adding a new task to the list'
+        return context
 
 
 class DetailsTaskView(generic.DetailView):
     model = Task
     template_name = 'DoIt/task_details.html'
+
+
+class TaskEditView(SuccessMessageMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+
+    def get_success_url(self):
+        messages.info(self.request, 'Task ' + self.request.POST['name'] + ' edited')
+        return reverse_lazy('DoIt:tasks', kwargs={'pk': get_object_or_404(Task, id=self.kwargs['pk']).list.id})
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskEditView, self).get_context_data(**kwargs)
+        context['list'] = get_object_or_404(List, id=get_object_or_404(Task, id=self.kwargs['pk']).list.id)
+        context['page_title'] = 'Editing task of'
+        return context
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+
+    def get_success_url(self):
+        task = get_object_or_404(Task, id=self.kwargs['pk'])
+        messages.info(self.request, 'Task ' + task.name + ' deleted successfully')
+        return reverse_lazy('DoIt:tasks', kwargs={
+            'pk': task.list.id
+        })
