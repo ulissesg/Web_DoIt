7fb54dcb3c0fcff46c34405b8e0851677b7d0e71
Ulisses G <ulissesg2018@gmail.com>
2021-08-04 15:29:27 -0300

    admin page redone

--- DoIt/admin.py
+++ DoIt/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 
 from DoIt.models import List, Task
 
-admin.site.register(List)
-admin.site.register(Task)
+
+class TaskInLine(admin.StackedInline):
+    model = Task
+    extra = 0
+    fieldsets = ('task', {
+        'classes': ('collapse',),
+        'fields': ('name', 'description', 'is_done', 'start_date', 'end_date', 'time_it_takes', 'is_important'),
+    }),
+
+
+class ListAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'user']}),
+    ]
+    inlines = [TaskInLine]
+    list_display = ('name', 'user')
+
+    search_fields = ['name']
+
+
+admin.site.register(List, ListAdmin)
